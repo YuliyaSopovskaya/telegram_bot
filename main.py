0c37cdc1bc01fb3fa8458f2e85b468be136bbb3c
“Yuliya” <enjoy2121@yandex.ru>
2022-11-10 01:27:03 +0300

    finish

--- main.py
+++ main.py
@@ -11,7 +11,21 @@ def get_weather(city, open_weather_token):
         f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
         )
         data = r.json()
-        pprint(data)
+        # pprint(data)
+        
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+
+
+        print(f"Погода в городе: {city}\n Температура: {cur_weather}"
+        f"Влажность: {humidity}\n Давление: {pressure} мм.рт.ст\n Ветер: {wind}"
+        f"Хорошего дня!"
+        )
+
+
     except Exception as ex:
         print(ex)
         print("Проверьте название города")
